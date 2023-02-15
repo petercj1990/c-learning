f53391ba93d542cab22928e19e36e7992a0d30b6
Peber Jensen <petetjensen@gmail.com>
2023-02-15 14:45:50 -0700

    adding in ch 3

---
+++ cs11dotnet7/chapter02/StoringNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿uint naturalNumber = 23;
+int intergernumber = -23;
+float realBumber = 2.3F;
+double anotherRealNmber = 2.3;
+int decimalnotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexNotation = 0x_001E_8480;
+
+Console.WriteLine($"{decimalnotation== binaryNotation }");
+
+Console.WriteLine($"{decimalnotation== hexNotation }");
+
+
+double a = 0.1;
+double b = 0.2;
+if (a + b == 0.3){
+    Console.WriteLine($"{a} + {b} equals {0.3}");
+}
+if (a + b != 0.3){
+    Console.WriteLine($"{a} + {b} does not equal {0.3}");
+}
---
+++ cs11dotnet7/chapter02/arguments/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace arguments.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
